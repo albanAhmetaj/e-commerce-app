ef6cc6b08d25e776118732437025f302011386c2
Alban Ahmetaj <alban@INTERADRIA.local>
2024-12-28 16:58:20 +0100

    initial commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
+
+@Component({
+  selector: 'app-header',
+  imports: [PrimaryButtonComponent],
+  template: `
+    <div class="bg-slate-100 px-4 py-4 shadow-md flex justify-between items-center">
+      <span class="text-xl">My App</span>
+      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+    </div>
+  `,
+  styles: ``,
+})
+export class HeaderComponent {
+
+  showButtonClicked() {
+    console.log('clicked');
+  }
+
+}
